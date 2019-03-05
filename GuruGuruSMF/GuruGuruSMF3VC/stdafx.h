9f71a2d9a563aab6303f328d020ea6e1945f7106
MihailJP <mihailjp@gmail.com>
2019-03-05 18:00:39 +0900

    Reconfigure for DXSDK August 2007

--- GuruGuruSMF/GuruGuruSMF3VC/stdafx.h
+++ GuruGuruSMF/GuruGuruSMF3VC/stdafx.h
@@ -8,20 +8,12 @@
 // 下で指定された定義の前に対象プラットフォームを指定しなければならない場合、以下の定義を変更してください。
 // 異なるプラットフォームに対応する値に関する最新情報については、MSDN を参照してください。
 #ifndef WINVER
-#define WINVER 0x0400
+#define WINVER 0x0501
 #endif
 
 #ifndef _WIN32_WINNT
-#define _WIN32_WINNT 0x0400
+#define _WIN32_WINNT 0x0501
 #endif						
-
-#ifndef _WIN32_WINDOWS
-#define _WIN32_WINDOWS 0x0410
-#endif
-
-#ifndef _WIN32_IE
-#define _WIN32_IE 0x0500
-#endif
 
 #define WIN32_LEAN_AND_MEAN		// Windows ヘッダーから使用されていない部分を除外します。
 // Windows ヘッダー ファイル:
